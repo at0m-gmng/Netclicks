ec55c5de9e5a5da175a06eaae366f43ed5033ad7
at0m-gmng <61505233+at0m-gmng@users.noreply.github.com>
2020-05-27 19:36:19 +0700

    update rating

--- script/main.js
+++ script/main.js
@@ -1,9 +1,60 @@
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
 const leftMenu = document.querySelector('.left-menu');
 const burger  = document.querySelector('.hamburger');
 const tvShowList =  document.querySelector('.tv-shows__list');
 const modal = document.querySelector('.modal');
-// const cross = document.querySelector('.cross');
 
+// загрузка из DB
+const DBService = class  {
+    getData = async (url) => {
+        const res = await fetch(url);
+        if(res.ok){
+            return res.json();
+        } else {
+            throw new Error(`Не удалось получить запрос по адресу ${url}`);
+        }
+    };
+    getTestData = async () => {
+        return await this.getData('test.json');
+    };
+}
+// создание карточек фильмов
+const renderCard =  (response) => {
+    response.results.forEach( ({ 
+        backdrop_path: backdrop, 
+        name: title, 
+        poster_path: poster, 
+        vote_average: vote
+    }) => {
+
+        // если нет постера, добавляем img  "нет постера"
+        const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+        // если нет backdrop, то ничего не выводим
+        const backdropIMG = backdrop ? IMG_URL + backdrop : ''; 
+        // если нет рейтинша, не выводим tv-card__vote
+        const voitValue = vote ? `<span class="tv-card__vote">${vote}</span>` :  '';
+    
+
+        const card = document.createElement('li');
+        card.classList.add('tv-shows__item');
+        card.innerHTML = ` 
+            <a href="#" class="tv-card">
+                ${voitValue}
+                <img class="tv-card__img"
+                    src="${posterIMG}"
+                    data-backdrop="${backdropIMG}"
+                    alt="${title}">
+                <h4 class="tv-card__head">${title}</h4>
+            </a>
+        `;
+        tvShowList.insertAdjacentElement('afterbegin', card);
+        // или
+        // tvShowList.append(card);
+
+    } );
+};
+
+new DBService().getTestData().then(renderCard);
 
 //открытие закрытие меню
 burger.addEventListener('click', () => {
@@ -27,7 +78,6 @@ leftMenu.addEventListener('click', (event) => {
         burger.classList.add('open');
     }
 });
-
 
 //Смена img карточек при наведении
 const changeImage = (event) => {
